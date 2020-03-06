56b21f05d50a4b59786b7bf1ed527f6acd3065f0
Jorge Mancero <jman0829@gmail.com>
2020-03-06 15:31:53 -0500

    updated

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,99 +1,36 @@
 var friendsData = [
-    {
-        "name":"John",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        "scores":[
-            5,
-            1,
-            4,
-            4,
-            5,
-            1,
-            2,
-            5,
-            4,
-            1
-          ]
-      },
-      {
-        "name":"Mary",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        "scores":[
-            5,
-            1,
-            2,
-            4,
-            3,
-            1,
-            2,
-            5,
-            1,
-            1
-          ]
-      },
-      {
-        "name":"Ahmed",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        "scores":[
-            5,
-            1,
-            4,
-            2,
-            5,
-            1,
-            2,
-            1,
-            4,
-            1
-          ]
-      },{
-        "name":"Shannon",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        "scores":[
-            5,
-            1,
-            2,
-            4,
-            5,
-            1,
-            2,
-            5,
-            3,
-            1
-          ]
-      },
-      {
-        "name":"Carlos",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        "scores":[
-            5,
-            1,
-            4,
-            1,
-            1,
-            1,
-            2,
-            2,
-            4,
-            1
-          ]
-      },
-      {
-        "name":"Eugenia",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        "scores":[
-            5,
-            4,
-            4,
-            4,
-            3,
-            1,
-            2,
-            2,
-            2,
-            1
-          ]
-      }
-];
+  {
+    "name": "John",
+    "photo":
+      "https://media3.giphy.com/media/XBplU1eNyUi9NrkkXb/giphy.gif?cid=790b7611f79caeda859c290ab3a2817533afdb3ea8571856&rid=giphy.gif",
+    "scores": [1, 2, 3, 4, 5, 1, 2, 5, 4, 4]
+  },
+  {
+    "name": "Crystal ",
+    "photo":
+      "https://media2.giphy.com/media/aCG04CXf6wX3q/200.webp?cid=790b761129f4c3f430c9294964dbef67c28dc9d5d4d35296&rid=200.webp",
+    "scores": [1, 2, 5, 1, 2, 2, 4, 1, 3, 2]
+  },
+  {
+    "name": "Irma",
+    "photo":
+      "https://media1.giphy.com/media/8bp98Q4HUQuNG/giphy.gif?cid=790b7611576cf0da67632f7933b4922e6fb152a7158f8b6a&rid=giphy.gif",
+    "scores": [1, 4, 2, 2, 5, 1, 3, 2, 5, 5]
+  },
 
-module.exports = friendsArray;
+  {
+    "name": "Kathy",
+    "photo":
+      "https://media3.giphy.com/media/4Ufg2FqPq3uCI/giphy.gif?cid=790b76111cc067b210d03c71fda7e10271de80a68846f93c&rid=giphy.gif",
+    "scores": [3, 3, 3, 2, 4, 3, 5, 3, 5, 4]
+  },
+  {
+    "name": "Bruce",
+    "photo":
+      "https://media3.giphy.com/media/l4EoWy3pJIIMXY2Ws/giphy.gif?cid=790b76110ab020fc987b8196fd34f88cd71aa9d2f01f333f&rid=giphy.gif",
+    "scores": [2, 4, 3, 3, 2, 2, 3, 2, 4, 5]
+  },
+
+
+];
+module.exports = friendsData;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,20 +1,49 @@
-
 var friendsData = require("../data/friends");
 
-module.exports = function(app) {
-    
-    app.get("/api/friends", function(req,res) {
-        res.json(friendsData);
-    });
+module.exports = function (app) {
 
-    app.post("/api/friends", function(req,res) {
-        friendsData.push(req.body);
-        res.json(true);
-    });
 
-    app.post("/api/clear", function(req,res) {
-        friendsData.length = 0;
-        res.json({ ok: true });
-    });
+  app.get("/api/friends", function (req, res) {
+    res.json(friendsData);
+  });
 
+
+
+
+  app.post("/api/friends", function (req, res) {
+    var currentUserData = req.body
+    var userScore = currentUserData.scores
+
+        var totalDifference;
+        var bestMatch = {
+            name: "",
+            picture: "",
+            difference: 100
+        };
+        for (var i = 0; i < friendsData.length; i++) {
+            totalDifference = 0;
+            for (var j = 0; j < friendsData[i].scores.length; j++) {
+                totalDifference += Math.abs(friendsData[i].scores[j] - userScore[j]);
+                // console.log(totalDifference)
+                if (totalDifference <= bestMatch.difference) {
+                    bestMatch.name = friendsData[i].name,
+                        bestMatch.picture = friendsData[i].photo,
+                        bestMatch.difference = totalDifference
+                        console.log("-------")
+                        console.log(bestMatch)
+                        console.log("-------")
+                }
+            }
+        };
+        friendsData.push(currentUserData);
+        res.json(bestMatch);
+
+  });
+
+
+
+  app.post("/api/clear", function (req, res) {
+    friendsData.length = 0;
+    res.json({ ok: true });
+  });
 };
